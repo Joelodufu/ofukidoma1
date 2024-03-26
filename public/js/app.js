6de37cfe53dfff54508433a4a1fc33db0c9cee90
joelodufu <joel.odufu@gmail.com>
2024-03-26 20:05:51 +0100

    firssss4

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,41 @@
 const projectList = document.querySelector('#card-deck');
-const projects= [
+const projects = [
     {
-        name :"Donation of K-yan Device",
-        id :"1",
-        date :"12/343",
-        venue :"Ochetoha Ki Doma",
-        about :"This project is aimed at enhancing the teaching delivery for schools for the handicaped and the ophanage home",
-        image :"./images/22.jpg",
-        images :[
+        name: "Distribution of sacks of garri cash to IDPs",
+        id: "4",
+        date: "July 14, 15, 2023",
+        venue: "APA, OTUKPO, & OJU",
+        about: "",
+        image: "./images/idp/3.jpg",
+        images: [
+            "./images/idp/1.jpg",
+            "./images/idp/2.jpg",
+            "./images/idp/3.jpg",
+            "./images/idp/4.jpg",
+            "./images/idp/5.jpg",
+            "./images/idp/6.pg",
+            "./images/idp/7.jpg",
+            "./images/idp/8.jpg",
+            "./images/idp/9.jpg",
+            "./images/idp/10.jpg",
+            "./images/idp/11.jpg",
+            "./images/idp/12.jpg",
+            "./images/idp/13.jpg",
+            "./images/idp/14.jpg",
+            "./images/idp/15.jpg",
+            "./images/idp/16.jpg",
+            "./images/idp/17.jpg",
+            "./images/idp/18.jpg",
+        ]
+    },
+    {
+        name: "Donation of K-yan Device",
+        id: "1",
+        date: "12/343",
+        venue: "Ochetoha Ki Doma",
+        about: "This project is aimed at enhancing the teaching delivery for schools for the handicaped and the ophanage home",
+        image: "./images/22.jpg",
+        images: [
             "./images/1.jpg",
             "./images/2.jpg",
             "./images/3.jpg",
@@ -35,13 +63,13 @@ const projects= [
         ]
     },
     {
-        name :"Donation of School Tools and kits",
-        id :"1",
-        date :"12/343",
-        venue :"Ochetoha Ki Doma",
-        about :"This project is aimed at providing the basic tools required for accademics for students",
-        image :"./images/32.jpg",
-        images :[
+        name: "Donation of School Tools and kits",
+        id: "2",
+        date: "12/343",
+        venue: "Ochetoha Ki Doma",
+        about: "This project is aimed at providing the basic tools required for accademics for students",
+        image: "./images/32.jpg",
+        images: [
             "./images/1.jpg",
             "./images/2.jpg",
             "./images/3.jpg",
@@ -69,13 +97,13 @@ const projects= [
         ]
     },
     {
-        name :"Provision of enhanced Farm products",
-        id :"1",
-        date :"12/343",
-        venue :"Ochetoha Ki Doma",
-        about :"This poject is aimed at enhancing farming and agriculture in the community",
-        image :"./images/24.jpg",
-        images :[
+        name: "Provision of enhanced Farm products",
+        id: "3",
+        date: "12/343",
+        venue: "Ochetoha Ki Doma",
+        about: "This poject is aimed at enhancing farming and agriculture in the community",
+        image: "./images/24.jpg",
+        images: [
             "./images/1.jpg",
             "./images/2.jpg",
             "./images/3.jpg",
@@ -101,105 +129,107 @@ const projects= [
             "./images/23.jpg",
             "./images/24.jpg",
         ]
-    }
+    },
+
+
 ]
 
 //create element and render projects
 
-function renderProjects(project){
+function renderProjects(project) {
     let colDiv = document.createElement('div');
-        colDiv.className = "col-md-3 col-sm-6";
-        colDiv.setAttribute('data-id', project.id)
+    colDiv.className = "col-md-3 col-sm-6";
+    colDiv.setAttribute('data-id', project.id)
 
-    let cardDiv =document.createElement('div');
-        cardDiv.className="card shadow";
-    
+    let cardDiv = document.createElement('div');
+    cardDiv.className = "card shadow";
+
     let cardImg = document.createElement('img');
-        cardImg.className = "card-img-top";
-        cardImg.setAttribute('src', project.image)
+    cardImg.className = "card-img-top";
+    cardImg.setAttribute('src', project.image)
 
     let cardBody = document.createElement('div');
-        cardBody.className = "card-body";
+    cardBody.className = "card-body";
 
 
     let cardTittle = document.createElement('h5');
-        cardTittle.className = "card-title";
-       cardTittle.innerText=project.name
+    cardTittle.className = "card-title";
+    cardTittle.innerText = project.name
 
 
 
 
     let cardAdress = document.createElement('p');
-        cardAdress.className = "card-text lol";
-        cardAdress.innerText=project.venue
+    cardAdress.className = "card-text lol";
+    cardAdress.innerText = project.venue
 
     let adressIcon = document.createElement('img');
-        adressIcon.setAttribute ("src","./images/icons/geo-alt-fill.svg");
+    adressIcon.setAttribute("src", "./images/icons/geo-alt-fill.svg");
 
 
 
     let cardAwardees = document.createElement('p');
-        cardAwardees.className = "card-text";
-        cardAwardees.innerText='Community Development';
+    cardAwardees.className = "card-text";
+    cardAwardees.innerText = 'Community Development';
 
     let awardeesIcon = document.createElement('img');
-        awardeesIcon.setAttribute ("src","./images/icons/people-fill.svg");
+    awardeesIcon.setAttribute("src", "./images/icons/people-fill.svg");
 
 
 
     let cardWorth = document.createElement('span');
-        cardWorth.className = "contiii";
-        cardWorth.innerText='455'
+    cardWorth.className = "contiii";
+    cardWorth.innerText = '455'
 
     let worthIcon = document.createElement('img');
-        worthIcon.setAttribute ("src","./images/icons/coin.svg");
+    worthIcon.setAttribute("src", "./images/icons/coin.svg");
 
 
 
     let cardDate = document.createElement('span');
-        cardDate.className = "contiii";
-        cardDate.innerText=project.date
+    cardDate.className = "contiii";
+    cardDate.innerText = project.date
 
     let dateIcon = document.createElement('img');
-        dateIcon.setAttribute ("src","./images/icons/calendar-event-fill.svg");
-    
-    
+    dateIcon.setAttribute("src", "./images/icons/calendar-event-fill.svg");
+
+
     let cardLink = document.createElement('a');
 
     let linkButton = document.createElement('button');
-        linkButton.className="btn btn-primary idbtn";
-        linkButton.innerText="See More"
-        linkButton.setAttribute('id', project.id)
-        linkButton.onclick=()=>{
-            localStorage.setItem('fileUrls', JSON.stringify(project.images));
-            localStorage.setItem('pname', JSON.stringify(project.name));
-            localStorage.setItem('adress', JSON.stringify(project.venue));
-            window.location.href="gallery.html"
-        }
+    linkButton.className = "btn btn-primary idbtn";
+    linkButton.innerText = "See More"
+    linkButton.setAttribute('id', project.id)
+    linkButton.onclick = () => {
+        localStorage.setItem('fileUrls', JSON.stringify(project.images));
+        localStorage.setItem('pname', JSON.stringify(project.name));
+        localStorage.setItem('adress', JSON.stringify(project.venue));
+        window.location.href = "gallery.html"
+    }
 
 
     let cardFooter = document.createElement('div')
-        cardFooter.className="card-footer"
+    cardFooter.className = "card-footer"
 
     let footNote = document.createElement("small")
-    footNote.className="text-muted"
-    footNote.innerText=`${project.about} Images/videos`
+    footNote.className = "text-muted"
+    footNote.innerText = `${project.about} Images/videos`
 
     //appending children
     colDiv.appendChild(cardDiv)
     cardDiv.appendChild(cardImg)
     cardDiv.appendChild(cardBody)
-        cardBody.appendChild(cardTittle)
-        cardBody.appendChild(cardAdress)
-            cardAdress.appendChild(adressIcon)
-        cardBody.appendChild(cardAwardees)
-            cardAwardees.appendChild(awardeesIcon)
-        cardBody.appendChild(cardDate)
-            cardDate.appendChild(dateIcon)
-        cardBody.appendChild(cardLink)
-            cardLink.appendChild(linkButton)
-        cardBody.appendChild(cardFooter)
-            cardFooter.appendChild(footNote)
+    cardBody.appendChild(cardTittle)
+    cardBody.appendChild(cardAdress)
+    cardAdress.appendChild(adressIcon)
+    cardBody.appendChild(cardAwardees)
+    cardAwardees.appendChild(awardeesIcon)
+    cardBody.appendChild(cardDate)
+    cardDate.appendChild(dateIcon)
+    cardBody.appendChild(cardLink)
+    cardLink.appendChild(linkButton)
+    cardBody.appendChild(cardFooter)
+    cardFooter.appendChild(footNote)
 
     projectList.appendChild(colDiv)
 }
@@ -207,7 +237,7 @@ function renderProjects(project){
 
 
 
-projects.forEach(project=>{
+projects.forEach(project => {
     renderProjects(project)
 })
 
